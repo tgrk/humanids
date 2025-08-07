42bbbca6f68d248bbb0651318b6d52fa074fa2fe
Martin Wiso <martin@wiso.cz>
2025-08-07 22:39:40 +0200

    Enable Styler formatter and update style

--- lib/humanids.ex
+++ lib/humanids.ex
@@ -10,7 +10,8 @@ defmodule HumanIDs do
   def generate(opts \\ []) do
     keys = HumanIDs.Sequences.keys()
 
-    Range.new(2, Keyword.get(opts, :segments, 3))
+    2
+    |> Range.new(Keyword.get(opts, :segments, 3))
     |> Enum.reduce([Integer.to_string(:rand.uniform(100))], fn _segment, acc ->
       [HumanIDs.Sequences.get_random(Enum.random(keys)) | acc]
     end)
